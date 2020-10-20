4b8159817fe80fd9164b4f82f23cc179ce9fbbbd
ASprojects <adam.stradomski0@gmail.com>
2020-10-20 18:01:06 +0200

    html changes

--- static/script.js
+++ static/script.js
@@ -1,6 +1,12 @@
 // $$$ SINGLE BUTTONS $$$
+function add_goback_button() {
+    window.location.href = "http://127.0.0.1:5001/"
+    }
+var go_back = document.getElementById("go_back")
+go_back.addEventListener("click", add_goback_button);
+
+
 function add_gotomenu_button() {
-    new XMLHttpRequest();
     window.location.href = "http://127.0.0.1:5001/summary_menu"
     }
 var summary_menu = document.getElementById("summary_menu")
@@ -8,7 +14,6 @@ summary_menu.addEventListener("click", add_gotomenu_button);
 
 
 function add_show_all_button() {
-    new XMLHttpRequest();
     window.location.href = "http://127.0.0.1:5001/show_all"
     }
 var show_all = document.getElementById("show_all")
@@ -16,19 +21,13 @@ show_all.addEventListener("click", add_show_all_button);
 
 
 function add_clear_data_button() {
-    new XMLHttpRequest();
     window.location.href = "http://127.0.0.1:5001/clear"
     }
 var clear_data = document.getElementById("clear_data")
 clear_data.addEventListener("click", add_clear_data_button);
 
 
-function add_goback_button() {
-    new XMLHttpRequest();
-    window.location.href = "http://127.0.0.1:5001/"
-    }
-var go_back = document.getElementById("go_back")
-go_back.addEventListener("click", add_goback_button);
+
 
 
 // $$$ FUNCTIONAL BUTTONS (calc) $$$
