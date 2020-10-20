2853f94723542d496b1332267b3b2c2cf576d8cd
ASprojects <adam.stradomski0@gmail.com>
2020-10-20 13:58:35 +0200

    templates up

--- static/script.js
+++ static/script.js
@@ -1,4 +1,12 @@
 // $$$ SINGLE BUTTONS $$$
+function add_gotomenu_button() {
+    new XMLHttpRequest();
+    window.location.href = "http://127.0.0.1:5001/summary_menu"
+    }
+var summary_menu = document.getElementById("summary_menu")
+summary_menu.addEventListener("click", add_gotomenu_button);
+
+
 function add_show_all_button() {
     new XMLHttpRequest();
     window.location.href = "http://127.0.0.1:5001/show_all"
@@ -15,4 +23,12 @@ var clear_data = document.getElementById("clear_data")
 clear_data.addEventListener("click", add_clear_data_button);
 
 
-// $$$ FUNCTIONAL BUTTONS (menu, calc) $$$
+function add_goback_button() {
+    new XMLHttpRequest();
+    window.location.href = "http://127.0.0.1:5001/"
+    }
+var go_back = document.getElementById("go_back")
+go_back.addEventListener("click", add_goback_button);
+
+
+// $$$ FUNCTIONAL BUTTONS (calc) $$$
